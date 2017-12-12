67f73f51b98231da11179b16beba57feb6f22ff4
vsgurung <vsgurung@yahoo.com>
2017-12-12 12:24:39 +0000

    Update dbutils.py

--- Discovery/dbutils.py
+++ Discovery/dbutils.py
@@ -26,7 +26,7 @@ def get_connection(conn_info):
 
 
 def get_sqlserver_connections():
-    """ Read SQL Server connection names from QSettings stored by QGIS
+    """ Read SQL Server connection names from QSettings stored by QGIS and returns list of database connections
     """
     settings = QSettings()
     settings.beginGroup(u"/MSSQL/connections/")
