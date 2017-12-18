6c8a38c2bc6c4971b05d63cae860702b4bd51a6c
vsgurung <vsgurung@yahoo.com>
2017-12-18 16:11:05 +0000

    Syntax change in list_schemas function.

--- Discovery/dbutils.py
+++ Discovery/dbutils.py
@@ -74,7 +74,7 @@ def list_schemas(in_cursor):
     """ Get list of schema names
     	   
     """
-    schemas = list(set([r.table_schem for r in in_cursor.tables().fetchall()])
+    schemas = [set([r.table_schem for r in in_cursor.tables().fetchall()])]
     if schemas:
         return sorted(schemas)
 
